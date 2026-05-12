454efcba93ce5e87fca78c6d60bc6f86033991cb
Yulia Carvalho <yuliya.carvalho@gmail.com>
2026-05-12 11:13:15 +0100

    Added dbt mart model and schema name macro

---
+++ dbt_project/models/mart/mart_observations.sql
@@ -0,0 +1,33 @@
+WITH staging AS (
+    SELECT *
+    FROM {{ ref('stg_observations') }}
+),
+
+stations AS (
+    SELECT
+        CAST(station_id AS INT64) AS station_id,
+        station_name,
+        CAST(latitude AS FLOAT64) AS latitude,
+        CAST(longitude AS FLOAT64) AS longitude
+    FROM `ipma-weather-496012.raw.stations`
+),
+
+final AS (
+    SELECT
+        s.observed_at,
+        s.station_id,
+        st.station_name,
+        st.latitude,
+        st.longitude,
+        s.temperature_c,
+        s.humidity_pct,
+        s.precipitation_mm,
+        s.wind_speed_kmh,
+        s.wind_direction,
+        s.pressure_hpa,
+        s.solar_radiation_kjm2
+    FROM staging s
+    LEFT JOIN stations st ON s.station_id = st.station_id
+)
+
+SELECT * FROM final
