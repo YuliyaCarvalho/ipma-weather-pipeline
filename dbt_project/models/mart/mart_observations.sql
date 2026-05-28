a7253677dd7e36b91f2ef284e617868ccd0a76b5
Yulia Carvalho <yuliya.carvalho@gmail.com>
2026-05-28 14:52:30 +0100

    added 7-region classification and solar index to mart_observations

--- dbt_project/models/mart/mart_observations.sql
+++ dbt_project/models/mart/mart_observations.sql
@@ -22,7 +22,12 @@ final AS (
         CASE
             WHEN st.longitude < -25 THEN 'Açores'
             WHEN st.latitude < 33.5 AND st.longitude < -15 THEN 'Madeira'
-            ELSE 'Continental'
+            WHEN st.latitude < 37.5 AND st.longitude < -7.4 THEN 'Algarve'
+            WHEN st.latitude < 38.5 AND st.longitude > -8.0 THEN 'Alentejo'
+            WHEN st.latitude BETWEEN 38.5 AND 39.5 THEN 'Lisboa e Vale do Tejo'
+            WHEN st.latitude BETWEEN 39.5 AND 40.5 THEN 'Centro'
+            WHEN st.latitude > 40.5 THEN 'Norte'
+            ELSE 'Centro'
         END AS region,
         s.temperature_c,
         s.humidity_pct,
@@ -30,7 +35,15 @@ final AS (
         s.wind_speed_kmh,
         s.wind_direction,
         s.pressure_hpa,
-        s.solar_radiation_kjm2
+        s.solar_radiation_kjm2,
+        CASE
+            WHEN s.solar_radiation_kjm2 IS NULL THEN NULL
+            WHEN s.solar_radiation_kjm2 / 3.6 < 200 THEN 'Low'
+            WHEN s.solar_radiation_kjm2 / 3.6 < 400 THEN 'Moderate'
+            WHEN s.solar_radiation_kjm2 / 3.6 < 600 THEN 'High'
+            WHEN s.solar_radiation_kjm2 / 3.6 < 800 THEN 'Very High'
+            ELSE 'Extreme'
+        END AS solar_index
     FROM staging s
     LEFT JOIN stations st ON s.station_id = st.station_id
 )
